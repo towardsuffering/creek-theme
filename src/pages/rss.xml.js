9edddb43129aee4e11c32d1eee53975dadcec759
towardsuffering <christopher.rotunno@outlook.com>
2024-02-01 19:25:31 -0600

    first commit

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss from "@astrojs/rss";
 
-const postImportResult = import.meta.globEager("./posts/*.md");
-const posts = Object.values(postImportResult);
+const postImportResult = import.meta.glob('./posts/*.md', { eager: true });const posts = Object.values(postImportResult);
 
 export const get = () =>
   rss({
-    title: "Chris Blogs",
-    description: "Chris Blogs",
+    title: "Astro Theme Creek",
+    description: "A Theme for Astro",
     site: import.meta.env.SITE,
     items: import.meta.glob("./posts/**/*.md"),
   });
